4474ef4a8190e80ec986fedc428cf81acbd2fbea
Li Haixin <lihaixin000@gmail.com>
2024-07-17 15:27:37 +0800

    added icons for files on view files page

--- frontend/src/pages/ViewFilesPage.js
+++ frontend/src/pages/ViewFilesPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { FileText, Image, Video, Music, File } from 'lucide-react';
 import '../pagesCss/ViewFilesPage.css';
 
 function ViewFilesPage() {
@@ -19,13 +20,41 @@ function ViewFilesPage() {
     fetchFiles();
   }, [API_BASE_URL]);
 
+  const getFileIcon = (fileName) => {
+    const fileExtension = fileName.split('.').pop().toLowerCase();
+    switch (fileExtension) {
+      case 'jpg':
+      case 'jpeg':
+      case 'png':
+      case 'gif':
+        return <Image />;
+      case 'mp4':
+      case 'mkv':
+      case 'avi':
+        return <Video />;
+      case 'mp3':
+      case 'wav':
+        return <Music />;
+      case 'pdf':
+      case 'doc':
+      case 'docx':
+      case 'txt':
+        return <FileText />;
+      default:
+        return <File />;
+    }
+  };
+
   return (
     <div className="view-files-page">
       <h2>Uploaded Files</h2>
       <ul>
         {files.map((file, index) => (
           <li key={index}>
-            <a href={file.url} target="_blank" rel="noopener noreferrer">{file.name}</a>
+            <a href={file.url} target="_blank" rel="noopener noreferrer">
+              {getFileIcon(file.name)}
+              <span>{file.name}</span>
+            </a>
           </li>
         ))}
       </ul>
